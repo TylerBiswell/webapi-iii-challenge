8046f704ea59fa8ea3c6c205ff17feece33710a6
Tyler Biswell <tsbiswell@gmail.com>
2019-10-03 12:43:33 -0600

    Built custom logger

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 // code away!
 const express = require('express');
-const helmet = require('helmet');
 
 const postRouter = require('./posts/postRouter');
 
 const server = express();
 
-// server.use(logger);
-server.use(helmet());
-server.use(express.json());
+server.use('/api/posts', postRouter);
 
 const port = 4000;
 server.listen(port, () =>
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,41 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.use(logger);
+router.use(helmet());
+router.use(express.json());
 
-});
+router.get('/', (req, res) => res.send(`<h1>Test</h1>`));
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {});
 
-});
+router.delete('/:id', (req, res) => {});
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 // custom middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin',
+    )}`,
+  );
 
-function validatePostId(req, res, next) {
+  next();
+}
 
-};
+function validateUserId(req, res, next) {
+  next();
+}
+
+function validateUser(req, res, next) {
+  next();
+}
+
+function validatePost(req, res, next) {
+  next();
+}
 
 module.exports = router;
