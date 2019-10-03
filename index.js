fdb92807bfed9028c753c8787cdde40226f5e850
Tyler Biswell <tsbiswell@gmail.com>
2019-10-03 12:41:41 -0600

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+const express = require('express');
+const helmet = require('helmet');
+
+const postRouter = require('./posts/postRouter');
+
+const server = express();
+
+// server.use(logger);
+server.use(helmet());
+server.use(express.json());
+
+const port = 4000;
+server.listen(port, () =>
+  console.log(`\n*** Server running on http://localhost:${port} ***\n`),
+);
